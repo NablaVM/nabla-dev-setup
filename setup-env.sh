f78b3d08ec7a6b6eb21a1b3f22f89f2694f33607
bosley <bosley117@gmail.com>
2020-05-21 17:42:40 -0400

    Created a basic setup script

---
+++ setup-env.sh
@@ -0,0 +1,67 @@
+
+#
+#   Get the path and make sure that the path exists
+#
+read -p  "Please enter the directory to set everything up in : " nablaPath
+
+if [ -d "${nablaPath}" ] 
+then
+    while true; do
+        read -p "Directory \"${nablaPath}\" already exists. Would you like to continue ? (y/n) : " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+else
+    while true; do
+        read -p "Directory \"${nablaPath}\" does not exists. Would you like to create it ? (y/n) : " yn
+        case $yn in
+            [Yy]* ) mkdir -p ${nablaPath} ; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+fi
+
+#
+#   Clone everything into place
+#
+cd "${nablaPath}"
+
+mkdir builds
+
+git clone https://github.com/NablaVM/libnabla 
+git clone https://github.com/NablaVM/solace 
+git clone https://github.com/NablaVM/nabla 
+
+cd builds
+mkdir nabla
+mkdir solace
+mkdir libnabla
+
+#
+#   Write a build script
+#
+cat <<EOT >> build-all.sh
+cd ${nablaPath}/builds/libnabla
+cmake ${nablaPath}/libnabla/lib
+make -j9
+
+sudo make install
+
+cd ${nablaPath}/builds/solace
+cmake ${nablaPath}/solace/src
+make -j9
+
+cd ${nablaPath}/builds/nabla
+cmake ${nablaPath}/nabla/src
+make -j9
+
+cd ${nablaPath}/builds
+EOT
+
+chmod +x build-all.sh
+
+echo -e "\n\nEverything is all set. You can now find the nabla code in the path you gave with a build directory and a fancy build script.\n"
